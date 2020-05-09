10742cf9a1b3abc3f2f199aca46b634eff822f26
Sameya Alam <sameya.alam@gmail.com>
2020-05-09 20:44:00 +0530

    added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """:return a list containing numbers"""
+
+        apiview_list = ['one',
+                        'two',
+                        'three',
+                        'four',
+        ]
+
+        return Response({'message':'hello!!','apiview_list':apiview_list})
